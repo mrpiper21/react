435585926fb7d46eaeb3f80889b2eb9456cc46a3
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2023-11-30 20:36:38 +0000

    Nested routes

--- test4/src/index.jsx
+++ test4/src/index.jsx
@@ -2,28 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from '../src/pages/Home'
+import About from '../src/pages/About'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />
 );
 
-function About () {
-  return (
-    <h1>About page</h1>
-  )
-}
-
-function Home () {
-  return (
-    <h1>Home page</h1>
-  )
-}
-
 function App() {
   return (
     <Router>
-      <Link to={'/'}>home</Link> <br />
-      <Link to={'/About'}>About</Link>
+        <Link to='/'>{Home}</Link>
+        <Link to='/About'>{About}</Link>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
