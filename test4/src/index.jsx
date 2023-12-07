213e7c2a95afc6070d0fdf507a08377e737969dc
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2023-12-07 19:25:06 +0000

    styling

--- test4/src/index.jsx
+++ test4/src/index.jsx
@@ -1,47 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { FaSignInAlt } from 'react-icons/fa'
 import logo from './pages/Images/Logo4.jpg'
-import './index.css';
+import React, { lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Posts from './pages/Posts';
-import Events from './pages/Events';
-import Sermons from './pages/Sermons';
-import Admin from './pages/Admin';
+import './index.css';
+
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Posts = lazy(() => import('./pages/Posts'));
+const Events = lazy(() => import('./pages/Events'));
+const Sermons = lazy(() => import('./pages/Sermons'));
+const Admin = lazy(() => import('./pages/Admin'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 function App() {
   return (
     <Router>
-      <header className="nav-bar">
-        <nav className='top-nav'>
+      <Suspense fallback={<div>Loading...</div>}>
+        <header className="nav-bar">
           <img src={logo} alt="img" className='logo'/>
-          <Link to="/admin" className='admin-panel'><FaSignInAlt /></Link>
-          <Link to="/" className="prop">Home</Link>
-          <Link to="/posts" className="prop">Posts</Link>
-          <Link to="/events" className="prop">Events</Link>
-          <Link to="/sermons" className="prop">Sermons</Link>
-          <Link to="/about" className="prop">About</Link>
-        </nav>
-        <nav className='left-nav'>
-          
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/sermons" element={<Sermons />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </main>
+          <div className='left-nav'>
+            <h2>Left Nav</h2>
+            <h2>Left Nav</h2>
+            <h2>Left Nav</h2>
+          </div>
+          <NavBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/sermons" element={<Sermons />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </main>
+      </Suspense>
     </Router>
   )
+}
+
+function NavBar() {
+  return (
+    <nav className='top-nav'>
+      <Link to="/admin" className='admin-panel'><FaSignInAlt /></Link>
+      <Link to="/" className="prop">Home</Link>
+      <Link to="/posts" className="prop">Posts</Link>
+      <Link to="/events" className="prop">Events</Link>
+      <Link to="/sermons" className="prop">Sermons</Link>
+      <Link to="/about" className="prop">About</Link>
+    </nav>
+  );
 }
 
 export default App;
