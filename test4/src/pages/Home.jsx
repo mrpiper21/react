1a055081f24153fc7718a5bb726ee63f383f5fe1
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2023-12-12 22:15:16 +0000

    Nav bar update

--- test4/src/pages/Home.jsx
+++ test4/src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home () {
         <div className='left-nav'>
           <img src={logo2} alt="img" className='logo2'/>
         </div>
-        <div className="main-pages">
+        <div className="home-div">
          <p>'Romans 15:13- May the God of hope fill you with all joy and peace as you trust in him, so that you may overflow with hope by the power of the Holy Spirit.Ephesians 3:20 - Now to him who is able to do immeasurably more than all we ask or imagine, according to his power that is at work within us. Romans 8:38-39'</p>
         </div>
       </>
