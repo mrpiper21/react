94f7021a5a45963448c5efbfe67b071e9f43e805
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2023-12-27 15:08:00 +0000

    calendar update

--- test4/src/pages/Calendar.jsx
+++ test4/src/pages/Calendar.jsx
@@ -4,30 +4,24 @@ import Eventday from './Eventday';
 const days = ['SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT'];
 
 const Calendar = ({ dates, handleDateClick, selectedDate, events }) => (
-    <>
-      {/* <div className='cal-days'>
-        
-      </div> */}
-      <div className="calendar">
-        <div>
-          {days.map((day, index) => (
-              <h6 className='cal-days' key={index}>{day}</h6>
-            ))}
-        </div>
-        {dates.map(({ date, currenMonth, today }) => (
-        <Eventday 
-          key={date} 
-          date={date} 
-          events={events}
-          currenMonth={currenMonth} 
-          today={today}
-          handleDateClick={handleDateClick} 
-          selectedDate={selectedDate}
-        />
-        ))}
-      </div>
-    </>
-
+  <div className="calendar">
+    <div className='cal-days'>
+      {days.map((day, index) => (
+        <h6 key={index}>{day}</h6>
+      ))}
+    </div>
+    {dates.map(({ date, currenMonth, today }) => (
+      <Eventday 
+        key={date} 
+        date={date} 
+        events={events}
+        currenMonth={currenMonth} 
+        today={today}
+        handleDateClick={handleDateClick} 
+        selectedDate={selectedDate}
+      />
+    ))}
+  </div>
 );
 
 export default Calendar;
--- test4/src/pages/Eventday.jsx
+++ test4/src/pages/Eventday.jsx
@@ -1,7 +1,7 @@
 import cn from './utils/Cn';
 import * as React from 'react';
 
-const Eventday = React.memo(({ date, currenMonth, events, today, selectedDate, handleDateClick }) => (
+const Eventday = React.memo(({ date, currenMonth, today, handleDateClick }) => (
   <button onClick={() => handleDateClick(date)} // Passing the date to handle click
               className={cn(currenMonth ? "current" : "" ,
               'calendar-btn',
